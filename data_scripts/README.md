a5c737672f57ed17eb2a9c150944af8e0e3916a6
Lawhy <yuan.he.oxcs@gmail.com>
2023-07-28 16:31:57 +0100

    update data scripts

--- data_scripts/README.md
+++ data_scripts/README.md
@@ -1,4 +1,12 @@
+### Dataset Construction Scripts
 
-This directory contains scripts for creating subsumption mappings and canddiate mappings.
+- `umls_scripts`: related to UMLS reference mapping and semantic type extraction.  
+- `generate_refs_subs.py`: used to generate reference subsumption mappings from a given set of reference equivalence mappings.
+- `generate_cand_maps.py`: used to generate candidate mappings for a given set of reference mappigns.
+for creating subsumption mappings and canddiate mappings.
 
-These scripts should only be used on the OAEI 2022 version as the OAEI 2023 version has enriched entities that should not be used in alignment.
+The above scripts are related to the OAEI 2022 version and cannot be directly used for the OAEI 2023 version because it has enriched entities that should not be used in alignment.
+
+### Dataset Display Scripts
+
+- `om_statistics.py`: used to display statistics about an OM pair.
---
+++ data_scripts/om_statistics.py
@@ -0,0 +1,27 @@
+from deeponto.onto import Ontology
+from deeponto.align.mapping import *
+import click
+from deeponto.utils import FileUtils
+
+@click.command()
+@click.option("-t", "--task_name", type=str)
+def generate_statistics(task_name: str):
+    src_onto_name, tgt_onto_name = task_name.split("-")
+    if "." in tgt_onto_name:
+        src_onto_name = src_onto_name + "." + tgt_onto_name.split(".")[-1]
+    src_onto = Ontology(f"{task_name}/{src_onto_name}.owl")
+    tgt_onto = Ontology(f"{task_name}/{tgt_onto_name}.owl")
+    refs_equiv = len(ReferenceMapping.read_table_mappings(f"{task_name}/refs_equiv/full.tsv"))
+    refs_subs = len(ReferenceMapping.read_table_mappings(f"{task_name}/refs_subs/train.tsv"))
+    refs_subs += len(FileUtils.read_table(f"{task_name}/refs_subs/test.cands.tsv"))
+    stats = {
+        "task": task_name,
+        "#SrcCls": len(src_onto.owl_classes),
+        "#TgtCls": len(tgt_onto.owl_classes),
+        "#RefsEquiv": refs_equiv,
+        "#RefsSubs": refs_subs
+    }
+    print(stats)
+
+if __name__ == "__main__":
+    generate_statistics()
